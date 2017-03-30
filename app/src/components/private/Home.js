99c75a9a3555ff56007120ad6d76dddc2ac1a1ce
quiaro <quiaro@gmail.com>
2017-03-30 00:03:27 -0600

    Add content to private home page

--- app/src/components/private/Home.js
+++ app/src/components/private/Home.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import { List, ListItem } from 'material-ui/List';
+import { Link } from 'react-router-dom';
 import PrivateAppBar from './AppBar'
 
 const PrivateHome = (props) => (
   <div>
     <PrivateAppBar user={props.currentUser} />
-    <div>Yay! The dashboard is displayed!!</div>
+    <div>What have others read lately?</div>
+    <List>
+      <ListItem><Link to={'/books'}>Books</Link></ListItem>
+      <ListItem><Link to={'/articles'}>Articles</Link></ListItem>
+      <ListItem><Link to={'/quotes'}>Quotes</Link></ListItem>
+    </List>
+    <div>
+      <RaisedButton label="Share what you have read" primary={true} />
+    </div>
   </div>
 )
 
