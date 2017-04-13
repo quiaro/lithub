3e9ff27b1c5e416f8094583567aa5608d51e7ef4
quiaro <quiaro@gmail.com>
2017-04-13 08:31:27 -0600

    Style book detail (book read by others)

--- app/src/components/BookView.js
+++ app/src/components/BookView.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
 import moment from 'moment';
+
+const buttonStyle = {
+  marginLeft: 15
+}
 
 const BookView = ({ book, history }) => {
   let body;
@@ -17,38 +22,59 @@ const BookView = ({ book, history }) => {
                         onTouchTap={() => { history.push('/books') }} />
           <RaisedButton label="Add To My History"
                         primary={true}
+                        style={buttonStyle}
                         onTouchTap={() => { history.push(`/add/book?title=${url_title}&author=${url_author}`) }} />
         </div>
-        <div className='details'>
-          <div className='title'>
-            <span className='label'>Title</span>
-            <span className='value'>{book.title}</span>
-          </div>
-          <div className='author'>
-            <span className='label'>Author</span>
-            <span className='value'>{book.author}</span>
-          </div>
-          <div className='record-date'>
-            <span className='label'>Last modified on: <i>{moment(book.last_modified).format('MMM D, YYYY')}</i></span>
-          </div>
-          {book.reviews.map( (review, index) => (
-            review ?
-              (<div key={index} className='review'>
-                <span className='rating'>{review.rating}</span>
-                {review.comments && <span className='value'>{review.comments}</span>}
-              </div>) : null
-          )) }
-        </div>
+
+        <main>
+          <section className='details'>
+            <div className='title'>
+              <b>Title</b>
+              <span>{book.title}</span>
+            </div>
+            <div className='author'>
+              <b>Author</b>
+              <span>{book.author}</span>
+            </div>
+            <span className='record-date'>Last modified on: <i>{moment(book.last_modified).format('MMM D, YYYY')}</i></span>
+          </section>
+
+          <section className='comments'>
+            <h2>Comments</h2>
+            {book.reviews.map( (review, index) => (
+              review ?
+                (<div key={index} className='review'>
+                  <span className='date'>On {moment(review.last_modified).format('MMM D, YYYY')}</span>
+                  <span className='rating'>{review.rating}</span>
+                  {review.comments && <blockquote>{review.comments}</blockquote>}
+                </div>) : null
+            )) }
+          </section>
+        </main>
+
+          {/* If there are more than 10 comments show action buttons at the bottom too so the
+          user doesn't have to scroll up */}
+
+          {book.reviews.length > 10 &&
+            <div className='actions'>
+              <RaisedButton label="Back to Book Listing"
+                            primary={true}
+                            onTouchTap={() => { history.push('/books') }} />
+              <RaisedButton label="Add To My History"
+                            primary={true}
+                            style={buttonStyle}
+                            onTouchTap={() => { history.push(`/add/book?title=${url_title}&author=${url_author}`) }} />
+            </div>
+          }
       </div>
     );
   }
 
   return (
-    <div>
+    <Paper className="others-detail">
       <h1>Book Detail</h1>
       {body}
-      <RaisedButton label="Back to Book Listing" primary={true} onTouchTap={() => { history.push('/books') }} />
-    </div>
+    </Paper>
   )
 }
 
