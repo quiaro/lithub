07dd73e6128c7429990331ef4862f29d3dfe96d0
quiaro <quiaro@gmail.com>
2017-04-14 14:46:09 -0600

    Fix event listener (monitorScroll) not being removed on unmount

--- app/src/containers/BooksByOthers.js
+++ app/src/containers/BooksByOthers.js
@@ -11,6 +11,34 @@ class BooksByOthers extends Component {
   constructor(props) {
     super(props);
     this.isScrolling = false;
+
+    // Fetch more data on scroll
+    this.monitorScroll = function(e) {
+      if (!this.isScrolling) {
+        // Scroll optimization with window.requestAnimationFrame
+        // per: https://developer.mozilla.org/en-US/docs/Web/Events/scroll
+        window.requestAnimationFrame(() => {
+          const htmlEl = document.querySelector('html');
+          const bodyEl = document.querySelector('body');
+
+          // If the user has scrolled past 80% of the body's scroll height
+          // (total height including overflow), then we'll make a request to
+          // fetch more books
+          const threshold = Math.round(bodyEl.scrollHeight * 0.8);
+          if (htmlEl.clientHeight + bodyEl.scrollTop > threshold) {
+            const { fetchReadByOthers, isFetchingBooksReadByOthers, booksNextIndex } = this.props;
+
+            // Make sure a request hasn't already been made before making
+            // another request
+            if (!isFetchingBooksReadByOthers) {
+              fetchReadByOthers(booksNextIndex);
+            }
+          }
+          this.isScrolling = false;
+        });
+      }
+      this.isScrolling = true;
+    }.bind(this)
   }
 
   componentDidMount() {
@@ -19,7 +47,7 @@ class BooksByOthers extends Component {
     // Every time the component is mounted, a request is made to fetch
     // all the books read by others
     fetchReadByOthers();
-    window.addEventListener('scroll', this.monitorScroll.bind(this));
+    window.addEventListener('scroll', this.monitorScroll);
   }
 
   componentWillUnmount() {
@@ -29,34 +57,7 @@ class BooksByOthers extends Component {
     // by other users so that when it is mounted again results won't
     // be stale.
     resetBooks();
-    window.removeEventListener('scroll', this.monitorScroll.bind(this));
-  }
-
-  monitorScroll(e) {
-    if (!this.isScrolling) {
-      // Scroll optimization with window.requestAnimationFrame
-      // per: https://developer.mozilla.org/en-US/docs/Web/Events/scroll
-      window.requestAnimationFrame(() => {
-        const htmlEl = document.querySelector('html');
-        const bodyEl = document.querySelector('body');
-
-        // If the user has scrolled past 80% of the body's scroll height
-        // (total height including overflow), then we'll make a request to
-        // fetch more books
-        const threshold = Math.round(bodyEl.scrollHeight * 0.8);
-        if (htmlEl.clientHeight + bodyEl.scrollTop > threshold) {
-          const { fetchReadByOthers, isFetchingBooksReadByOthers, booksNextIndex } = this.props;
-
-          // Make sure a request hasn't already been made before making
-          // another request
-          if (!isFetchingBooksReadByOthers) {
-            fetchReadByOthers(booksNextIndex);
-          }
-        }
-        this.isScrolling = false;
-      });
-    }
-    this.isScrolling = true;
+    window.removeEventListener('scroll', this.monitorScroll);
   }
 
   render() {
